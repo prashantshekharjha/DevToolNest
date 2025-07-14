43d36c5a7436e74af22e43148061d46bd4ca0699
Prashant Jha <prashantshekhar.usa@gmail.com>
2025-07-14 12:51:18 -0500

    Fix light mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -55,7 +55,7 @@ function App() {
     <WouterRouter base="/DevToolNest">
       <ThemeProvider
         attribute="class"
-        defaultTheme="dark"
+        defaultTheme="light"
         enableSystem
         disableTransitionOnChange
       >
